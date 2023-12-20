026524a61c20dab114d4f26890cd5159ce321b7b
iwakiXPS <iwaki.rrc10@gmail.com>
2023-12-21 00:03:28 +0900

    feat: dupulicate_sumo完成

--- tool/duplicate_sumo.py
+++ tool/duplicate_sumo.py
@@ -8,11 +8,14 @@ import xml.etree.ElementTree as ET
 # ファイルの読み込みとxmlツールの設定
 
 datasource_dir = './datasource'
-output_dir = './sumo_xml'
+output_rootdir = './sumo_xml'
 
 scenarios = ['direct', 'curve_r150', 'curve_r60', 'curve_r40', 'curve_r30']
 
 for scenario in scenarios:
+
+    output_dir = os.path.join(output_rootdir, scenario)
+    os.mkdir(output_dir)
 
     net_xml_filename = os.path.join(datasource_dir, scenario+'.net.xml')
     rou_xml_filename = os.path.join(datasource_dir, scenario+'.rou.xml')
@@ -28,17 +31,19 @@ for scenario in scenarios:
 
     # sumoファイルの複製
 
-    for speed in range(0, 30, 2):
+    for max_speed in range(0, 30, 2):
         for accel in [0, 1, 2]:
 
             # 車道の最高速度の設定。
-            root_net[1][0].attrib['speed'] = str(speed)
+            root_net[1][0].attrib['speed'] = str(max_speed)
 
             # 車両の出発位置。とりあえず０
-            root_rou[1].attrib['depart'] = '0.00'
+            depart_position = '0.00'
+            root_rou[1].attrib['depart'] = depart_position
 
             # 車両の出発速度。とりあえず０
-            root_rou[1].attrib['departSpeed'] = '0.00'
+            depart_speed = '0.00'
+            root_rou[1].attrib['departSpeed'] = depart_speed
 
             # 車両の加速度
             root_rou[0].attrib['accel'] = str(accel)
@@ -53,7 +58,7 @@ for scenario in scenarios:
             root_rou[0].attrib['maxSpeed'] = '300.00'
 
             # sumocfgに書かれるファイル名
-            filename_suffix = '_s{}_a{}'.format(speed, accel)
+            filename_suffix = '_ms{}_ac{}_dp{}_ds{}'.format(max_speed, accel, depart_position, depart_speed)
 
             root_cfg[0][0].attrib['value'] = scenario+filename_suffix+'.net.xml'
             root_cfg[0][1].attrib['value'] = scenario+filename_suffix+'.rou.xml'
