41d03b65a102083364ead33da9281673949a1e35
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-01-01 22:41:11 +0900

    pythonを途中まで翻訳

---
+++ beamtracking/modules.py
@@ -0,0 +1,21 @@
+import parameters
+
+class Unit:
+    def __init__(self, n_elx, n_elz):
+        self.n_elx = n_elx
+        self.n_elz = n_elz
+        self.n_elall = n_elx * n_elz
+
+        self.dx = parameters.LMD / 2
+        self.dz = parameters.LMD / 2
+        self.ox = 0
+        self.oz = 0
+
+
+class DU(Unit):
+    def __init__(self, n_elx, n_elz):
+        super().__init__(n_elx, n_elz)
+
+class RU(Unit):
+    def __init__(self, n_elx, n_elz):
+        super().__init__(n_elx, n_elz)
