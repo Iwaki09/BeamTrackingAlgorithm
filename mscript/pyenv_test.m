2826acf571acc7175d07596e773586196617eacb
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-01-28 17:42:21 +0900

    重要な変更はなし

--- mscript/pyenv_test.m
+++ mscript/pyenv_test.m
@@ -1,13 +1,16 @@
 clear all;
-terminate(pyenv)
-% pyenv(ExecutionMode="InProcess")
+% terminate(pyenv)
+pyenv(ExecutionMode="OutOfProcess")
 % pe = pyenv(Version="/Users/iwakiryo2/.pyenv/versions/3.10.12/bin/python")
-pe = pyenv("Version", "/Users/iwakiryo2/.pyenv/shims/python")
+% pe = pyenv("Version", "/usr/bin/python3")
+pe = pyenv("Version", "/Library/Developer/CommandLineTools/usr/bin/python3")
+% pe = pyenv ("Version", "/Users/iwakiryo2/.pyenv/shims/python")
 % pyrunfile("pyenv_test.py", "df")
 % py = pyenv;
 % pe.Version
+% py.sys.path
 
-% search_way = pyrunfile("svm_for_matlab.py", "res", dist=20, speed=20, accel=20)
+search_way = pyrunfile("svm_for_matlab.py", "res", dist=20, speed=20, accel=20)
 % pyenv(ExecutionMode="OutOfProcess")
-py.list({"Monday","Tuesday","Wednesday","Thursday","Friday"});
+% py.list({"Monday","Tuesday","Wednesday","Thursday","Friday"});
 % pyrun(["print('Hello World')"])
