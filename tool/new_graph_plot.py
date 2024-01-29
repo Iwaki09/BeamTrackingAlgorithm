88d64480df920471aa447fc618197c5ca044a4dc
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-01-29 23:06:47 +0900

    graph_plotを改良

--- tool/new_graph_plot.py
+++ tool/new_graph_plot.py
@@ -13,14 +13,18 @@ def plot_main():
 
     scenario = scenarios[2]
 
+    linewidth = 2
+
     # 1: NoMLで単体 2: NoMLで全部(未完成) 11: MLをplot
-    plot_mode = 1
+    plot_mode = 11
 
     if plot_mode == 1:
-        plot_individual_normal(os.path.join(input_dir1, scenario))
+        plot_individual_normal(os.path.join(input_dir1, scenario), linewidth)
+    elif plot_mode == 11:
+        plot_individual_ml(input_dir1, input_dir2, scenario, output_dir, linewidth)
 
 
-def plot_individual_normal(scenario):
+def plot_individual_normal(scenario, linewidth):
     df_2way = pd.read_csv(scenario+'_2way.csv', names=['x', 'SNR_2way', 'SNR_o', 'SNR_s'])
     df_2dim = pd.read_csv(scenario+'_2dim.csv', names=['x', 'SNR_2dim', 'SNR_o', 'SNR_s'])
     df_4way = pd.read_csv(scenario+'_4way.csv', names=['x', 'SNR_4way', 'SNR_o', 'SNR_s'])
@@ -33,11 +37,11 @@ def plot_individual_normal(scenario):
             min_df_x = df['x']
 
     if (min_df_x.equals(dfs[0]['x'])):
-        df_non = pd.read_csv(scenario_now+'_2way2.csv', names=['x', 'SNR_non'])
+        df_non = pd.read_csv(scenario+'_2way2.csv', names=['x', 'SNR_non'])
     elif (min_df_x.equals(dfs[1]['x'])):
-        df_non = pd.read_csv(scenario_now+'_2dim2.csv', names=['x', 'SNR_non'])
+        df_non = pd.read_csv(scenario+'_2dim2.csv', names=['x', 'SNR_non'])
     else:
-        df_non = pd.read_csv(scenario_now+'_4way2.csv', names=['x', 'SNR_non'])
+        df_non = pd.read_csv(scenario+'_4way2.csv', names=['x', 'SNR_non'])
 
     df_2way = df_2way.iloc[:len(min_df_x)]
     df_2dim = df_2dim.iloc[:len(min_df_x)]
@@ -68,6 +72,72 @@ def plot_individual_normal(scenario):
     plt.grid(alpha=0.3)
     plt.legend(loc='upper right')
     # plt.savefig(output_dir+scenario_now+'_SNR_all.pdf')
+
+    # df_all.to_csv(output_dir+scenario_now+'_all.csv')
+    # df_non.to_csv(output_dir+scenario_now+'_all2.csv')
+
+    plt.show()
+
+    plt.clf()
+
+
+def plot_individual_ml(input_dir1, input_dir2, scenario, output_dir, linewidth):
+    scenario_path1 = os.path.join(input_dir1, scenario)
+    scenario_path2 = os.path.join(input_dir2, scenario)
+
+    df_2way = pd.read_csv(scenario_path1+'_2way.csv', names=['x', 'SNR_2way', 'SNR_o', 'SNR_s'])
+    df_2dim = pd.read_csv(scenario_path1+'_2dim.csv', names=['x', 'SNR_2dim', 'SNR_o', 'SNR_s'])
+    df_4way = pd.read_csv(scenario_path1+'_4way.csv', names=['x', 'SNR_4way', 'SNR_o', 'SNR_s'])
+    df_ml = pd.read_csv(scenario_path2+'_ml.csv', names=['x', 'SNR_ml', 'SNR_o', 'SNR_s'])
+
+    dfs = [df_2way, df_2dim, df_4way, df_ml]
+
+    min_df_x = dfs[0]['x']
+    for df in dfs:
+        if len(df['x']) < len(min_df_x):
+            min_df_x = df['x']
+
+    if (min_df_x.equals(dfs[0]['x'])):
+        df_non = pd.read_csv(scenario_path1+'_2way2.csv', names=['x', 'SNR_non'])
+    elif (min_df_x.equals(dfs[1]['x'])):
+        df_non = pd.read_csv(scenario_path1+'_2dim2.csv', names=['x', 'SNR_non'])
+    elif (min_df_x.equals(dfs[2]['x'])):
+        df_non = pd.read_csv(scenario_path1+'_4way2.csv', names=['x', 'SNR_non'])
+    else:
+        df_non = pd.read_csv(scenario_path2+'_ml2.csv', names=['x', 'SNR_non'])
+
+    df_2way = df_2way.iloc[:len(min_df_x)]
+    df_2dim = df_2dim.iloc[:len(min_df_x)]
+    df_4way = df_4way.iloc[:len(min_df_x)]
+    df_ml = df_ml.iloc[:len(min_df_x)]
+
+    df_all = pd.DataFrame({
+        'x': df_2way['x'],
+        'SNR_opt': df_2way['SNR_o'],
+        'SNR_con': df_2way['SNR_2way'],
+        'SNR_swe': df_2way['SNR_s'],
+        'SNR_2dim': df_2dim['SNR_2dim'],
+        'SNR_4way': df_4way['SNR_4way'],
+        'SNR_ml': df_ml['SNR_ml'],
+    })
+
+    # print(df_2dim)
+    plt.plot(df_2way['x'], df_2way['SNR_o'], label='Optimal', color = 'blue', linewidth=linewidth)
+    plt.plot(df_2way['x'], df_2way['SNR_2way'], label='Conventional', color = 'orange', linewidth=linewidth)
+    plt.plot(df_2way['x'], df_2way['SNR_s'], label='Sweeping', color = 'green', linewidth=linewidth)
+    plt.plot(df_2dim['x'], df_2dim['SNR_2dim'], label='2dim', color = 'red', linewidth=linewidth)
+    plt.plot(df_4way['x'], df_4way['SNR_4way'], label='4way', color = 'pink', linewidth=linewidth)
+    plt.plot(df_ml['x'], df_ml['SNR_ml'], label='SVM', color = 'purple', linewidth=linewidth)
+    plt.plot(df_non['x'], df_non['SNR_non'], color = 'black', linewidth=linewidth)
+    plt.xlabel('position[m]')
+    plt.ylabel('SNR[dB]')
+    plt.xlim(5, 60.1)
+
+    plt.xticks([5,10,20,30,40,50,60]) 
+    plt.ylim(0, 60)
+    plt.grid(alpha=0.3)
+    plt.legend(loc='upper right')
+    plt.savefig(os.path.join(output_dir, scenario+'_SNR_all.pdf'))
 
     # df_all.to_csv(output_dir+scenario_now+'_all.csv')
     # df_non.to_csv(output_dir+scenario_now+'_all2.csv')
