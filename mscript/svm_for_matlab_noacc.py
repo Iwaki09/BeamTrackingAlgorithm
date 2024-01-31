3de770e50aa5fc9e3cc7ee15a620194f02b798c2
iwakiXPS <iwaki.rrc10@gmail.com>
2024-01-31 21:36:00 +0900

    guideで最も近いのを取れるように

--- mscript/svm_for_matlab_noacc.py
+++ mscript/svm_for_matlab_noacc.py
@@ -10,8 +10,9 @@ def svm_for_matlab_noacc(model_basename, scenario, x, speed):
 
     # ガイドファイルを読み込んで、xからdistとangleを計算
     df_guide = pd.read_csv(os.path.join(ml_models_dir, scenario+'_guide.csv'), names=['x', 'y', 'angle'])
-    y = df_guide[df_guide['x'] == x]['y'].values[0]
-    angle = df_guide[df_guide['x'] == x]['angle'].values[0]
+    nearest_x = df_guide.iloc[(df_guide['x'] - x).abs().idxmin()]['x']
+    y = df_guide[df_guide['x'] == nearest_x]['y'].values[0]
+    angle = df_guide[df_guide['x'] == nearest_x]['angle'].values[0]
     dist = np.sqrt((30-x)**2 + (y+5)**2)
 
     # 保存したモデルをロード
@@ -24,7 +25,7 @@ def svm_for_matlab_noacc(model_basename, scenario, x, speed):
     [dist, speed, angle] = ([dist, speed, angle] - stats[1]) / stats[0]
     # デバッグ用
     tmp = [dist, speed, angle]
-    print(stats[0])
+    # print(stats[0])
 
     data = np.array([dist, speed, angle]).reshape(1, -1)
     prediction = svm_model.predict(data)
@@ -37,4 +38,4 @@ def svm_for_matlab_noacc(model_basename, scenario, x, speed):
     return [search_way, tmp]
 
 res = svm_for_matlab_noacc(model_basename, scenario, x, speed)
-# res = svm_for_matlab_noacc('svm_noacc_basic', 'curve_r60', 5.2122554813724, 10)
+# res = svm_for_matlab_noacc('svm_noacc_basic', 'curve_r60', 5, 10)
--- mscript/svm_for_matlab_noacc_noguide.py
+++ mscript/svm_for_matlab_noacc_noguide.py
@@ -10,7 +10,8 @@ def svm_for_matlab_noacc_noguide(model_basename, scenario, x, y, speed):
 
     # ガイドファイルを読み込んで、xからdistとangleを計算
     df_guide = pd.read_csv(os.path.join(ml_models_dir, scenario+'_guide.csv'), names=['x', 'y', 'angle'])
-    angle = df_guide[df_guide['x'] == x]['angle'].values[0]
+    nearest_x = df_guide.iloc[(df_guide['x'] - x).abs().idxmin()]['x']
+    angle = df_guide[df_guide['x'] == nearest_x]['angle'].values[0]
     dist = np.sqrt((30-x)**2 + (y+5)**2)
 
     # 保存したモデルをロード
@@ -23,7 +24,7 @@ def svm_for_matlab_noacc_noguide(model_basename, scenario, x, y, speed):
     [dist, speed, angle] = ([dist, speed, angle] - stats[1]) / stats[0]
     # デバッグ用
     tmp = [dist, speed, angle]
-    print(stats[0])
+    # print(stats[0])
 
     data = np.array([dist, speed, angle]).reshape(1, -1)
     prediction = svm_model.predict(data)
