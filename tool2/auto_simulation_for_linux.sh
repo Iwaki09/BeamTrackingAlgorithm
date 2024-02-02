19a213579c09c6d0ef9c3081cdc455528d944b16
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-02-02 21:33:31 +0900

    重複削除

--- tool2/auto_simulation_for_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scenario="korakuen"
-sumo-gui --remote-port 8813 -c ./datasource/${scenario}.sumocfg &
-matlab -nodesktop -nosplash -sd "./mscript" -batch "beamtracking_ml('${scenario}_ml')"
--- tool2/auto_simulation_for_mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scenario="korakuen"
-sumo --remote-port 8813 -c ./datasource/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}_2dim'); exit;"
