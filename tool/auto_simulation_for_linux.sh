d7ef55bb6a042f7bc6f46d3897d683fae3691a4e
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-02-11 21:53:50 +0900

    paris

--- tool/auto_simulation_for_linux.sh
+++ tool/auto_simulation_for_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scenario="curve_r40"
+scenario="paris"
 sumo --remote-port 8813 -c ./datasource/${scenario}.sumocfg &
-matlab -nodesktop -nosplash -sd "./mscript" -batch "beamtracking_ml('${scenario}_ml_ver2')"
+matlab -nodesktop -nosplash -sd "./mscript" -batch "beamtracking_ml('${scenario}_ml_ver1')"
