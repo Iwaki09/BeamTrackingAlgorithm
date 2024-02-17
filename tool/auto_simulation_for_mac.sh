aeff82648be8d5ffd762f7938743a169906dac9c
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-02-17 11:27:57 +0900

    korakuen

--- tool/auto_simulation_for_mac.sh
+++ tool/auto_simulation_for_mac.sh
@@ -2,4 +2,4 @@
 
 scenario="korakuen"
 sumo --remote-port 8813 -c ./datasource/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}_ml_ver4'); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}_ml_ver3'); exit;"
