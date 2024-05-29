66b98835cdb779e765ae36e6358e8927c0024465
岩城燎 <iwakiryo2@iwakiniabinoAir.lan>
2024-05-30 00:02:20 +0900

    new_data

--- tool/rcs2406_batch3.sh
+++ tool/rcs2406_batch3.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-scenario="korakuen"
+scenario="charles"
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-svm_generic-type1-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-svm_korakuen-type1-ver1', 0, 1, 11, '../result', 0); exit;"
 
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-svm_generic-type2-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-svm_korakuen-type2-ver1', 0, 1, 11, '../result', 0); exit;"
 
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-dt_generic-type1-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-dt_korakuen-type1-ver1', 0, 1, 11, '../result', 0); exit;"
 
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-dt_generic-type2-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-dt_korakuen-type2-ver1', 0, 1, 11, '../result', 0); exit;"
 
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-xgb_generic-type1-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-xgb_korakuen-type1-ver1', 0, 1, 11, '../result', 0); exit;"
 
 sumo --remote-port 8813 -c ./sumo/original/${scenario}.sumocfg &
-/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-xgb_generic-type2-ver1', 0, 1, 11, '../result', 0); exit;"
+/Applications/MATLAB_R2023a.app/bin/matlab -nodesktop -nosplash -sd "~/Documents/01Research/01Source/BeamTrackingAlgorithm/mscript" -batch "beamtracking_ml('${scenario}-ml-xgb_korakuen-type2-ver1', 0, 1, 11, '../result', 0); exit;"
